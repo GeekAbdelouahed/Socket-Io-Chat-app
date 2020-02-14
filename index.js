ef904ae5cde485aebd987f5da4b5cdc0c3dd6ef1
Abdelouahed <medjoudja@sindibadinternational.net>
2020-02-14 21:06:06 +0100

    typing messages status

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ socketio.on("connection", (userSocket) => {
     userSocket.on("send_message", (data) => {
         userSocket.broadcast.emit("receive_message", data)
     })
+
+    userSocket.on("typing", (data) => {
+        userSocket.broadcast.emit("typing", data)
+    })
+
+    userSocket.on("stop_typing", (data) => {
+        userSocket.broadcast.emit("stop_tying", data)
+    })
 })
 
 http.listen(process.env.PORT)
