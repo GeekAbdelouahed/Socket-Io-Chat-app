52db4e10800e0beb333cc1be6b54789bb3ede339
Abdelouahed <medjoudja@sindibadinternational.net>
2020-02-14 10:31:13 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const app = require('express')()
+const http = require('http').createServer(app)
+const socketio = require('socket.io')(http)
+
+app.get('/', (req, res) => {
+    res.send("Server is running")
+})
+
+socketio.on("connection", (userSocket) => {
+    userSocket.on("send_message", (data) => {
+        userSocket.emit("receive_message", data)
+    })
+})
+
+http.listen(process.env.PORT)
